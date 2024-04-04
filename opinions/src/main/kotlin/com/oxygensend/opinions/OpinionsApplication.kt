f723822593f3222d85bdc1b48cfee768802b8411
oxygensend <szymon19314@wp.pl>
2024-04-04 21:44:18 +0200

    opinions: initialize opinions service

---
+++ opinions/src/main/kotlin/com/oxygensend/opinions/OpinionsApplication.kt
@@ -0,0 +1,11 @@
+package com.oxygensend.opinions
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OpinionsApplication
+
+fun main(args: Array<String>) {
+	runApplication<OpinionsApplication>(*args)
+}
