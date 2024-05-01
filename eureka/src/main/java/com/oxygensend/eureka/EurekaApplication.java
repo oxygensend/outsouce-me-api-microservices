e6bbcb4118229adaf1e6e4455792a688f267971f
oxygensend <szymon19314@wp.pl>
2024-05-01 09:42:32 +0200

    eureka server

---
+++ eureka/src/main/java/com/oxygensend/eureka/EurekaApplication.java
@@ -0,0 +1,15 @@
+package com.oxygensend.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaApplication.class, args);
+	}
+
+}
