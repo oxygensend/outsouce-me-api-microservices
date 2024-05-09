edd4ce23bb8d47e42fd59493caa5da28574a9eb5
oxygensend <szymon19314@wp.pl>
2024-05-09 20:50:06 +0200

    user-profile: validation changes

--- user-profile/src/main/java/com/oxygensened/userprofile/infrastructure/AppConfiguration.java
+++ user-profile/src/main/java/com/oxygensened/userprofile/infrastructure/AppConfiguration.java
@@ -1,9 +1,23 @@
 package com.oxygensened.userprofile.infrastructure;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.openapitools.jackson.nullable.JsonNullableModule;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @EnableConfigurationProperties( {SpringUserProfileProperties.class})
 @Configuration
 public class AppConfiguration {
+
+    @Bean
+    ObjectMapper objectMapper() {
+        var mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        mapper.registerModule(new JsonNullableModule());
+        return mapper;
+    }
 }
